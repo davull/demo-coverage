8bccb22d96a38fbb659a4307f6a49ccb037b193e
David Ullrich <david.ullrich@ullrich-software.de>
2023-03-17 22:10:17 +0100

    Init

---
+++ WebApplication1.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebApplication1.Tests.Controllers;
+
+public class WeatherForecastControllerTests
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public WeatherForecastControllerTests()
+    {
+        _factory = new WebApplicationFactory<Program>();
+    }
+
+    [Test]
+    public async Task Get_ReturnsOk()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/WeatherForecast");
+
+        response.EnsureSuccessStatusCode();
+    }
+}
